8a2d31d041b34f019bf54abc501fc71192701b05
Arlsik <murmillo228@gmail.com>
2024-11-28 02:47:21 +0500

    fixes for vercel

--- components/DoctorsCarousel.tsx
+++ components/DoctorsCarousel.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Image from "next/image";
-import {useState} from "react";
 import {Carousel, CarouselContent, CarouselItem} from "./ui/carousel";
 
 export default function DoctorsCarousel() {
